ae9098c1007bec868d469bccb8b71178c8fbd2d9
Hlxrs <0000122546030xsp@al.educacao.sp.gov.br>
2024-11-21 18:08:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'BTS',
+    'Qual o acompanhamento preferido de Jung HoSeok do BTS?',
+    'Kimchi'
+)
+
+criaCartao(
+    'GOT7',
+    'Qual dos membros tem o nome artístico solo de Team Wang?',
+    'Jackson Wang'
+)
+
+criaCartao(
+    'EXO',
+    'Qual intregrante atuou no drama "pure love"?',
+    'Do Kyung-Soo'
+)
+
+criaCartao(
+    'SUPER JUNIOR',
+    'Qual música do grupo fez mais sucesso?',
+    '"Sorry, Sorry"'
+)
